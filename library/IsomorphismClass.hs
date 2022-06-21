e2fc4626b1cc99feb8428858cfeb6cab3aba2c10
Nikita Volkov <nikita.y.volkov@mail.ru>
2022-06-21 16:35:00 +0300

    Rewrite the docs

--- library/IsomorphismClass.hs
+++ library/IsomorphismClass.hs
@@ -1,37 +1,69 @@
 -- |
--- = The ultimate solution to the Conversion Problem
+-- Isomorphism law as a lawful solution to the conversion problem.
 --
--- By Conversion Problem we mean a repetitive chain of robotic actions one has
--- to take to translate data from one form to another without losing
--- information. This problem is often extended by packages using different
--- conventions. This library addresses all those problems by abstracting over
--- patterns and providing a uniform and concise interface. Unlike with many
--- other conversion libraries this interface is also lawful, making it clear
--- and predictable.
+-- = Conversion problem
 --
--- How often do you import @Data.Text.Lazy@ only to call
--- its 'Data.Text.Lazy.fromStrict'? How about importing @Data.Text@ only to
--- to call its 'Data.Text.unpack'? How about going thru the always fun
--- sequence of importing @Data.ByteString.Builder@ only to to call its
+-- Have you ever looked for a @toString@ function? How often do you
+-- import @Data.Text.Lazy@ only to call its 'Data.Text.Lazy.fromStrict'? How
+-- about importing @Data.Text@ only to to call its 'Data.Text.unpack'? How
+-- about going thru the always fun sequence of
+-- importing @Data.ByteString.Builder@ only to to call its
 -- 'Data.ByteString.Builder.toLazyByteString' and then importing
 -- @Data.ByteString.Lazy@ only to call its 'Data.ByteString.Lazy.toStrict'?
 --
 -- Those all are instances of one pattern. They are conversions between
--- representations of the same information. Information being the same
--- implies that none of it gets lost during the conversions. The zero loss
--- can be proven by being able to restore data identical to the original from
--- its transformed representation.
+-- representations of the same information. Codebases that don't attempt to
+-- abstract over this pattern tend to be sprawling with this type of
+-- boilerplate. It's noise to the codereader, it's a burden to the
+-- implementor and the maintainer.
 --
--- Turns out there usually can only be one way of defining such a conversion
--- between two types. This makes a huge deal in terms of how clear it is what
--- the conversion does. It helps the library authors ensure that they define
--- something that makes sense and provides insight to the users about what
--- the conversions do. We capture this with a lawful class, which ensures
--- that there is only one proper way of defining an instance for it. However
--- despite the strict rules, there's a lot of instances of this pattern. So
--- it is both universal and clear.
+-- = Why another conversion library?
 --
--- Here's a few samples of what this library lets you do:
+-- Many libraries exist that approach the conversion problem. However all of
+-- them provide lawless typeclasses leaving it up to the author of the
+-- instance to define what makes a proper conversion. This results in
+-- inconsistencies across instances and their behaviour being not evident to
+-- the user.
+--
+-- This library tackles this problem with a lawful typeclass, making it
+-- evident what any of its instances do.
+--
+-- = The law
+--
+-- The key insight of this library is that if you add a requirement for the
+-- conversion to be lossless and to have a mirror conversion in the opposite
+-- direction, there usually appears to be only one way of defining it. That
+-- makes it very clear what the conversion does to the user and how to define
+-- it to the author of the conversion.
+--
+-- That insight itself stems from an observation that almost all of the
+-- practical conversions in Haskell share a property: you can restore the
+-- original data from its converted form. E.g., you can get a bytestring from
+-- a builder and you can create a builder from a bytestring, you can convert
+-- a text into a list of chars and vice-versa, bytestring to\/from bytearray,
+-- strict bytestring to\/from lazy, list to\/from sequence, sequence to/from
+-- vector, set of ints to\/from int-set. In other words, it's always a two-way
+-- street with them and there's a lot of instances of this pattern.
+--
+-- = UX
+--
+-- A few other accidental findings like encoding this property with recursive
+-- typeclass constraints and fine-tuning for the use of
+-- the @TypeApplications@ extension resulted in a very terse yet clear API.
+--
+-- Essentially the whole API is just two functions: 'to' and 'from'. Both
+-- perform a conversion between two types. The only difference between them
+-- is in what the first type application parameter specifies. E.g.:
+--
+-- > fromString = from @String
+--
+-- > toText = to @Text
+--
+-- In other words 'to' and 'from' let you explicitly specify either the source
+-- or the target type of a conversion when you need to help the type
+-- inferencer.
+--
+-- Here are more practical examples:
 --
 -- @
 -- renderNameAndHeight :: 'Text' -> 'Int' -> 'Text'
