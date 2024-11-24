eadf18f469a40c0531f5750cbd73acc454a3b6d4
Nikita Volkov <nikita.y.volkov@mail.ru>
2024-11-24 23:59:37 +0300

    Update the dep on quickcheck-instances

--- test/Test/ExtraInstances.hs
+++ test/Test/ExtraInstances.hs
@@ -4,15 +4,9 @@ module Test.ExtraInstances () where
 
 import qualified Data.ByteString.Builder as ByteStringBuilder
 import qualified Data.Text.Lazy.Builder as TextLazyBuilder
-import qualified Data.Vector.Generic as VectorGeneric
-import qualified Data.Vector.Primitive as VectorPrimitive
 import Rebase.Prelude
 import Test.QuickCheck
 import Test.QuickCheck.Instances ()
-
-instance (Arbitrary a, VectorPrimitive.Prim a) => Arbitrary (VectorPrimitive.Vector a) where
-  arbitrary = arbitraryVector
-  shrink = shrinkVector
 
 instance Arbitrary TextLazyBuilder.Builder where
   arbitrary = TextLazyBuilder.fromText <$> arbitrary
@@ -24,9 +18,3 @@ instance Eq ByteStringBuilder.Builder where
 instance Arbitrary ByteStringBuilder.Builder where
   arbitrary = ByteStringBuilder.byteString <$> arbitrary
   shrink = shrinkMap ByteStringBuilder.lazyByteString ByteStringBuilder.toLazyByteString
-
-arbitraryVector :: (VectorGeneric.Vector v a, Arbitrary a) => Gen (v a)
-arbitraryVector = VectorGeneric.fromList <$> arbitrary
-
-shrinkVector :: (VectorGeneric.Vector v a, Arbitrary a) => v a -> [v a]
-shrinkVector = fmap VectorGeneric.fromList . shrink . VectorGeneric.toList
