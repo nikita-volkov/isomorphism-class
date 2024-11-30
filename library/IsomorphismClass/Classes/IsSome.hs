524e0222e555ec5da2b6064a43c3ea1bdb1b8a77
nikita-volkov <nikita-volkov@users.noreply.github.com>
2024-11-30 08:53:01 +0000

    Format

--- library/IsomorphismClass/Classes/IsSome.hs
+++ library/IsomorphismClass/Classes/IsSome.hs
@@ -24,7 +24,7 @@ import IsomorphismClass.Prelude
 -- > \a -> maybeFrom (to a) == Just a
 --
 -- === Testing
--- 
+--
 -- For testing whether your instances conform to these laws use 'IsomorphismClass.isSomeLawsProperties'.
 class IsSome sup sub where
   -- |
