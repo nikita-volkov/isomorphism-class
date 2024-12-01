2d64232a4865c093860e607da55e592e8264d87f
Nikita Volkov <nikita.y.volkov@mail.ru>
2024-12-01 14:42:12 +0300

    Move away from autoexporter, because Hackage fails builds with it

--- library/IsomorphismClass/Classes.hs
+++ library/IsomorphismClass/Classes.hs
@@ -1,1 +1,4 @@
-{-# OPTIONS_GHC -F -pgmF autoexporter #-}
+module IsomorphismClass.Classes (module Exports) where
+
+import IsomorphismClass.Classes.Is as Exports
+import IsomorphismClass.Classes.IsSome as Exports
