e288267c7575fb9058789d997a6544b3c49148cc
Nikita Volkov <nikita.y.volkov@mail.ru>
2025-04-07 17:46:38 +0300

    Polish

--- library/IsomorphismClass/Relations/LazyTextAndLazyTextBuilder.hs
+++ library/IsomorphismClass/Relations/LazyTextAndLazyTextBuilder.hs
@@ -5,7 +5,6 @@ module IsomorphismClass.Relations.LazyTextAndLazyTextBuilder where
 import qualified Data.Text.Lazy
 import qualified Data.Text.Lazy.Builder
 import IsomorphismClass.Classes
-import IsomorphismClass.Prelude
 
 instance IsomorphicTo Data.Text.Lazy.Text Data.Text.Lazy.Builder.Builder where
   to = Data.Text.Lazy.Builder.toLazyText
